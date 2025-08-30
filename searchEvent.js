7e19336b568cac86c3ff1e55809b55885d55cc35
DC <debayudh.maldaitbss@gmail.com>
2025-08-30 13:15:28 +0530

    pankaj da incoming

--- searchEvent.js
+++ searchEvent.js
@@ -1,19 +1,19 @@
-    /*
-    1. Get the data
-    2. Get the selectedOption
-    3. Select table body
-    4. Empty Table body
-    5. filter the events based on selectedOption
-    6. For each element in event genrate the tr tag
-    7. append the tr tag to table body
-    8. use document.ready
-    9. use onchange in html/js
-    */
+/*
+1. Get the data
+2. Get the selectedOption
+3. Select table body
+4. Empty Table body
+5. filter the events based on selectedOption
+6. For each element in event generate the tr tag
+7. append the tr tag to table body
+8. use document.ready
+9. use onchange in html/js
+*/
 
 $(document).ready(filterTable);
 
 function parseDate(dateStr) {
-    const defaultDate = new Date(1972,2,1);
+    const defaultDate = new Date(1972, 2, 1);
     if (!dateStr) return defaultDate;
     if (dateStr.trim() === "") return defaultDate;
     const dateParts = dateStr.split("/");
@@ -23,20 +23,38 @@ function parseDate(dateStr) {
     return inputDate;
 }
 
-function filterTable(){
+function filterTable() {
     const events = JSON.parse(localStorage.getItem("events")) || [];
-    
+
     let selectedOption = $('#select').val();
     console.log(selectedOption);
     let tableBody = $('#listing');
 
     $('#listing').empty();
-    events.filter(function(evnt){
-        console.log(evnt);
+
+        let eventsFilter = events.filter((evnt) => {
         const dateTime = parseDate(evnt['dateTime']);
-        console.log(dateTime); 
+        console.log(dateTime);
         const now = new Date();
-        if 
+        console.log(selectedOption);
+        if (selectedOption === "future") {
+            if (dateTime >= now) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (dateTime < now) {
+                return true;
+            } else {
+                return false;
+            };
+        };
+    });
+
+    eventsFilter.forEach(items=> {
+        
     })
-}
+} 
+
 
