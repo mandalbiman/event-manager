d6da573cf6626239af1a0458fc31c780b161d27f
Nabin94 <Nabinkrmisra94@gmail.com>
2025-08-30 13:22:25 +0530

    all updated files has pulled

---  cardlist.js
+++  cardlist.js
@@ -1,84 +1,47 @@
-const sampleData = function() {
-    return `[
-        { 
-            "title": "ICC World Cup",
-            "date": "15th August 2027",
-            "description": "Tournament by ICC"
-        },
-        { 
-            "title": "Asia Cup",
-            "date": "20th July 2027",
-            "description": "Regional cricket tournament"
-        },
-        { 
-            "title": "Champions Trophy",
-            "date": "10th June 2027",
-            "description": "ICC One-Day tournament"
-        },
-        { 
-            "title": "T20 World Cup",
-            "date": "5th October 2027",
-            "description": "T20 format world championship"
-        },
-        { 
-            "title": "The Ashes",
-            "date": "1st December 2027",
-            "description": "England vs Australia test series"
-        },
-        { 
-            "title": "Border-Gavaskar Trophy",
-            "date": "15th January 2028",
-            "description": "India vs Australia test series"
-        },
-        { 
-            "title": "CPL Final",
-            "date": "18th September 2027",
-            "description": "Caribbean Premier League Final"
-        },
-        { 
-            "title": "Big Bash Final",
-            "date": "10th February 2028",
-            "description": "Australian Big Bash League Final"
-        },
-        { 
-            "title": "PSL Final",
-            "date": "25th March 2028",
-            "description": "Pakistan Super League Final"
-        },
-        { 
-            "title": "IPL Final",
-            "date": "28th May 2028",
-            "description": "Indian Premier League Final"
-        }
-    ]`;
-}
+$(document).ready(loadEvents)
 
+function loadEvents() {
+    // 1. Read the events from local storage with key events
 
+    let events = localStorage.getItem("events");
+    console.log(`Step 1: Events from localStorage ${events}`);
 
-function getCard(title, date, description) {
-    return `<div class="card">
+    // 2. convert the value into JSON object. 
+    const eventsObject = JSON.parse(events) || [];
+    console.log(`step 2: converted the key value ${eventsObject}`);
+
+    // 3. select the cardlist and empty the inner html
+    $("#card-list").empty();
+
+    // 4. forEach event in events get name, date & description
+    eventsObject.forEach(event => {
+        console.log(event);
+        const eventName= event['eventName'];
+        const eventdate= event['dateTime'];
+        const description= event['eventDescription'];
+        console.log(eventName);
+        console.log(eventdate);
+        console.log(description);
+
+        const eventCard= ` 
+        <div class="card">
                 <div class="card-contents">
                     <div class="card-icon">
                         <i class="fa-solid fa-circle-info"></i>
                     </div>
                     <div class="card-section">
-                        <h1>${title}</h1>
-                        <h3>${date}</h3>
+                        <h1>${eventName}</h1>
+                        <h2>${eventdate}</h2>
                         <p>${description}</p> <br>
                         <button class="card-action"> Read More</button>
                     </div>
                 </div>
-            </div>`
-}
+            </div>
+        `
 
-window.onload = function () {
-    let cardListEle = document.getElementById('card-list');
-    cardListEle.innerHTML = ""
-    let cardList = localStorage.getItem('card-list')
-    if (cardList === null || cardList.length === 0){
-        cardList = sampleData();
-    }
-    for (let card of JSON.parse(cardList)){
-        cardListEle.innerHTML += getCard(card['title'], card['date'], card['description'])
-    }
+        $('#card-list').append(eventCard);
+    });
+
+
+
 }
